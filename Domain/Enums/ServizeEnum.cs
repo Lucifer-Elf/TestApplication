1950c6b559c29ca2a390f0e282e6d111c357ff4f
Alfred Paul <alfredvpaul@hotmail.com>
2021-02-10 18:53:54 +0400

    Added Servize.com

---
+++ Domain/Enums/ServizeEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servize.Domain.Enums
+{
+    public class ServizeEnum
+    {
+        public enum Categories
+        { 
+         AUTOMOTIVE,
+         HOUSING,         
+         PERSONAL,         
+        }
+
+        public enum PackageType
+        { 
+         FREE,
+         BRONZE,
+         GOLD
+        }
+
+    }
+}
---
+++ Domain/Model/ServizeProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Servize.Domain.Enums.ServizeEnum;
+
+namespace Servize.Domain.Model
+{
+    public class ServizeProvider
+    {
+        public int           Id             { get; set; }
+
+        public string        CompanyName    { get; set; }
+
+        public string        FirstName      { get; set; }
+
+        public string        LastName       { get; set; }
+
+        public string        UserName       { get; set; }
+
+        public string        Email          { get; set; }
+
+        public string        Password       { get; set; }
+
+        public int           PhoneNumber    { get; set; }
+
+        public string        Address        { get; set; }  // Interact with google Api 
+
+        public string        Postal         { get; set; }
+
+        public Categories   Category        { get; set; }
+
+        public PackageType  PackageType     { get; set; }
+
+    }
+}
